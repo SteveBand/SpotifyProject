4298625ddfcc3fb5bcdc952e61abd8b6d7ab8727
SteveBand <steve020629@icloud.com>
2022-09-11 18:35:04 +0300

    first commit

--- src/main/cardsLoby/cardsLoby.js
+++ src/main/cardsLoby/cardsLoby.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useContext } from "react";
 import { CardsHipHop, CardsMood, CardsRock, CardsUpdated } from "./spotifyApi";
 import { Modal } from "../cardsLoby/modal";
@@ -28,34 +27,3 @@ export const CardsLoby = () => {
     </div>
   );
 };
-=======
-import React, { useEffect, useContext } from "react";
-import { CardsHipHop, CardsMood, CardsRock, CardsUpdated } from "./spotifyApi";
-import { Modal } from "../cardsLoby/modal";
-import { SignIn } from "../spotifylogin";
-import { Content } from "../../context";
-
-export const CardsLoby = () => {
-  const { showModal, setShowModal, token } = useContext(Content);
-
-  if (token) {
-    console.log(token);
-  }
-  useEffect(() => {}, [token]);
-  return (
-    <div>
-      {!token ? (
-        <SignIn />
-      ) : (
-        <>
-          <CardsUpdated />
-          <CardsMood />
-          <CardsHipHop />
-          <CardsRock />
-          <Modal />
-        </>
-      )}
-    </div>
-  );
-};
->>>>>>> fb76bbd39e2dc5799870514b55808637c8a6d9c3
